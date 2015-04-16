3a44631af51b84be6b2181f170d449cc57bee170
AlexSchiffke <alexander.schiffke@t-online.de>
2015-04-16 11:11:55 +0200

    Scanner erfragt jetzt, ob Abitur vorhanden oder nicht.

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,6 +3,9 @@ public class Studienberatung{
 	
 	public static void main(String[]args) {
 		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Hast du Abitur? j/n");
+		String answer = scanner.next();
 	}
 	
 }
