3468a1c17b8b1ada12c537174a793951da039c3c
AlexSchiffke <alexander.schiffke@t-online.de>
2015-04-16 11:26:55 +0200

    Frage nach Berufsausbildung hinzugefügt

--- Studienberatung.java
+++ Studienberatung.java
@@ -22,6 +22,14 @@ public class Studienberatung{
 			String answer = scanner.next();
 
 			if(answer.equals("j")){
+				System.out.println("Hast du eine Berufsausbildung? j/n");
+				String answer = scanner.next();
+
+				if(answer.equals("j")){
+
+				} else if (answer.equals("n")){
+
+			}
 
 			} else if (answer.equals("n")){
 
